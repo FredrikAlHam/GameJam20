6ba60afcb60ddc464ad7101f9525f8e6b3e40849
Rixipixi <56590257+Rixipixi@users.noreply.github.com>
2020-02-01 17:21:51 +0100

    done easterefdbmkrs

--- GameJam20/Assets/Scripts/FaceEasterEgg.cs
+++ GameJam20/Assets/Scripts/FaceEasterEgg.cs
@@ -20,7 +20,7 @@ public class FaceEasterEgg : MonoBehaviour
     IEnumerator waitFace()
     {
         face.enabled = true;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.3f);
         face.enabled = false;
     }
 }
