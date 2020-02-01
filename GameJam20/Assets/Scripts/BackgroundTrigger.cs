4fd243d3afccc6c6387d1ccd783cbf704774a032
Rixipixi <56590257+Rixipixi@users.noreply.github.com>
2020-02-01 11:37:17 +0100

    somethin here and there

--- GameJam20/Assets/Scripts/BackgroundTrigger.cs
+++ GameJam20/Assets/Scripts/BackgroundTrigger.cs
@@ -22,7 +22,7 @@ public class BackgroundTrigger : MonoBehaviour
         background5 = GameObject.Find("Background5").GetComponent<Image>();
         background6 = GameObject.Find("Background6").GetComponent<Image>();
         backgroundBlack = GameObject.Find("BackgroundBlack").GetComponent<Image>();
-       // Globals.time = 5;
+        Globals.time = 5;
     }
 
     void Update()
@@ -88,7 +88,7 @@ public class BackgroundTrigger : MonoBehaviour
         backgroundOff.enabled = false;
         backgroundBlack.enabled = true;
         footsteps.Play(0);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1.5f);
         backgroundBlack.enabled = false;
         backgroundOn.enabled = true;
         backgroundBlack.enabled = false;
