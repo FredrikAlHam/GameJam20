ce76406503d519cbdd45a4d3153a6645ffc9a3f8
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-02-01 12:55:22 +0100

    Added box elements

---
+++ GameJam20/Assets/Scripts/BoxOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxOpen : MonoBehaviour
+{
+    List<GameObject> screws = new List<GameObject>();
+    // Update is called once per frame
+    void Update()
+    {
+        screws.Clear();
+        foreach (Transform transform in gameObject.GetComponentsInChildren<Transform>())
+        {
+            if (transform.gameObject.name.Contains("Screw")) screws.Add(transform.gameObject);
+        }
+        if (screws.Count <= 0) Destroy(gameObject);
+    }
+}
