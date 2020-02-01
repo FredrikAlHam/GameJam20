1999c33b2687fd31f1a2bc273122f2609bfdd624
leoekstamelev <56590136+leoekstamelev@users.noreply.github.com>
2020-02-01 08:36:10 +0100

    Massa saker

---
+++ GameJam20/Assets/Leos saker, rör inte/Cable.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cable : MonoBehaviour
+{
+    bool cableGrabbed;
+    public int cableDestination, cableOrigin, color;
+    SpriteRenderer sprite;
+    Collider2D cableCollider;
+    LineRenderer cableLine;
+    Color red, blue, green;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        cableCollider = GetComponent<Collider2D>();
+        cableLine = GetComponent<LineRenderer>();
+        cableLine.SetPosition(0, transform.position);
+        cableLine.SetPosition(1, transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        cableLine.SetPosition(1, transform.position);
+
+        #region Color shit
+
+        if (color == 1)
+        {
+            sprite.color = Color.red;
+        }
+
+        else if (color == 2)
+        {
+            sprite.color = Color.blue;
+        }
+
+        else if (color == 3)
+        {
+            sprite.color = Color.green;
+        }
+
+        else if (color == 4)
+        {
+            sprite.color = Color.white;
+        }
+
+        #endregion
+
+        if (cableGrabbed)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        cableGrabbed = true;
+    }
+
+    void OnMouseUp()
+    {
+        cableGrabbed = false;
+        CheckCorrectPlacement();
+    }
+
+    #region Methods
+
+    void CheckCorrectPlacement()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (!cableGrabbed)
+        {
+            transform.position = col.transform.position;
+        }
+    }
+
+    #endregion
+}
---
+++ GameJam20/Assets/Leos saker, rör inte/CableManager5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableManager5 : MonoBehaviour
+{
+    public GameObject cable1, cable2, cable3, cable4, cable5;
+    int randomColor;
+
+
+    void Start()
+    {
+        cable1.GetComponent<Cable>().color = newRandomNumber();
+        cable2.GetComponent<Cable>().color = newRandomNumber();
+        cable3.GetComponent<Cable>().color = newRandomNumber();
+        cable4.GetComponent<Cable>().color = newRandomNumber();
+        cable5.GetComponent<Cable>().color = newRandomNumber();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    int newRandomNumber()
+    {
+        return randomColor = Random.Range(1, 5);
+    }
+}
